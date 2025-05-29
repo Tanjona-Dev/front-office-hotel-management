8f9a7c7e7504248a27739c365ca6803db2dafbab
Tanjona-Dev <rabemangasoatanjonanasa@gmail.com>
2025-05-29 22:10:50 +0300

    Add autoPlay from video

--- src/Pages/Landing-page/index.jsx
+++ src/Pages/Landing-page/index.jsx
@@ -1,10 +1,10 @@
-import videoLandingPage from "../../assets/teste.mp4"
+import landingPageVideo from "../../assets/video/teste.mp4"
 
 function LandingPage() {
   return (
     <div>
       <h1>Landing</h1>
-      <video src={videoLandingPage}  autoPlay loop  ></video>
+      <video src={landingPageVideo} muted autoPlay loop  ></video>
     </div>
   );
 }
